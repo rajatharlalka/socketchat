1b8966bf6bfdc6db3e5959bac6051829251d54f9
Rajat Harlalka <rajatharlalka@Rajats-MacBook-Pro.local>
2017-08-14 17:46:36 +0530

    Added app functionality

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('User Connected...');
+    
+    socket.on('disconnect', () => {
+        console.log('User Disconnected...');
+    });
+    
+    socket.on('add-message', (message, username) => {
+        io.emit('message', {type: 'new-message', text: message, username: username});
+    });
+});
+
+http.listen(8000, () => {
+    console.log('Server running on port 8000');
+});
--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -25,7 +25,8 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'socket.io-client':           'npm:vendor/socket.io-client'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -39,7 +40,8 @@
       },
       rxjs: {
         defaultExtension: 'js'
-      }
+      },
+      'socket.io-client':  {main: 'socket.io.js'}
     }
   });
 })(this);
